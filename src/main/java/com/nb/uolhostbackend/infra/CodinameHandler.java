880bacb3289bb7a67629b3b6c27b754261433e9d
Nicolas Bueno <nicolasbrian202@gmail.com>
2023-12-23 08:47:01 -0300

    logica de settar o codiname

---
+++ src/main/java/com/nb/uolhostbackend/infra/CodinameHandler.java
@@ -0,0 +1,24 @@
+package com.nb.uolhostbackend.infra;
+
+import com.nb.uolhostbackend.model.GroupType;
+import com.nb.uolhostbackend.service.CodinameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CodinameHandler {
+
+    @Autowired
+    private CodinameService codinameService;
+
+    public String findCodiname(GroupType groupType) {
+        if(groupType == GroupType.AVANGERS){
+            String firstMatch = codinameService.getAvangersCodinameList().stream().findFirst().orElseThrow();
+            this.codinameService.getAvangersCodinameList().remove(firstMatch);
+            return firstMatch;
+        }
+        String firstMatch = codinameService.getJusticeLeagueList().stream().findFirst().orElseThrow();
+        this.codinameService.getJusticeLeagueList().remove(firstMatch);
+        return firstMatch;
+    }
+}
--- src/main/java/com/nb/uolhostbackend/service/PlayerService.java
+++ src/main/java/com/nb/uolhostbackend/service/PlayerService.java
@@ -1,5 +1,7 @@
 package com.nb.uolhostbackend.service;
 
+import com.nb.uolhostbackend.infra.CodinameHandler;
+import com.nb.uolhostbackend.model.GroupType;
 import com.nb.uolhostbackend.model.Player;
 import com.nb.uolhostbackend.model.dtos.PlayerDto;
 import com.nb.uolhostbackend.repository.PlayerRepository;
@@ -11,8 +13,18 @@ public class PlayerService {
     @Autowired
     private PlayerRepository playerRepository;
 
+    @Autowired
+    private CodinameHandler codinameHandler;
+
     public Player createPlayer(PlayerDto dto){
         Player newPlayer = new Player(dto);
+        String codiname = getCodiname(dto.groupType());
+        newPlayer.setCodiname(codiname);
         return playerRepository.save(newPlayer);
     }
+
+    public String getCodiname(GroupType groupType){
+        return codinameHandler.findCodiname(groupType);
+    }
+
 }
